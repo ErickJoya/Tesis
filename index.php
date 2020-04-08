e30456841847b44144a75a4c5deb8e01b3be1f69
Erick Joya <erick-1996-@hotmail.com>
2020-04-08 08:45:48 -0600

    Agregue un parrafo al archivo

--- index.php
+++ index.php
@@ -9,11 +9,6 @@ echo "<h2> Mi primer cambio</h2>";
 
 echo "<p> Github es la PLATAFORMA online para trabajar con proyectos en git, 
 en simples palabras es utilizar git de forma remota pero entiendase 
-<<<<<<< HEAD
-que son cosas totamente DESIGUALES </p>";
-=======
 que son cosas totamente DIFERENTES </p>";
->>>>>>> ramaDouglas
-
 
 ?>
