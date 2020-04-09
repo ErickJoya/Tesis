68287817c529f45b9e1bbd6eb10f0e21cdceb35b
Erick Joya <erick-1996-@hotmail.com>
2020-04-08 19:01:48 -0600

    Arregle unas cuestiones de ahi

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<html>
+    <head>
+
+   </head>
+
+   <body>
 <?php
 include_once 'includes/user.php';
 include_once 'includes/user_session.php';
@@ -18,21 +27,32 @@ if(isset($_SESSION['user'])){
 
     $user = new User();
     if($user->userExists($userForm, $passForm)){
-        //echo "Existe el usuario";
+        
         $userSession->setCurrentUser($userForm);
         $user->setUser($userForm);
 
         include_once 'vistas/home.php';
     }else{
-        //echo "No existe el usuario";
         $errorLogin = "Nombre de usuario y/o password incorrecto";
+       ?>
+      
+    <div class="alert alert-dark" role="alert">
+    Nombre de usuario y/o password incorrecto
+    </div>
+
+ <?php 
+        
         include_once 'vistas/login.php';
     }
 }else{
-    //echo "login";
+  
     include_once 'vistas/login.php';
 }
 
 
 
 ?>
+
+<body>
+
+</html>
--- vistas/home.php
+++ vistas/home.php
@@ -1,3 +1,8 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -5,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
     <div id="menu">
@@ -14,9 +19,11 @@
             <li class="cerrar-sesion"><a href="includes/logout.php">Cerrar sesión</a></li>
         </ul>
     </div>
-
     <section>
-        <h1>Bienvenido <?php echo $user->getNombre();  ?></h1>
+    <div class="alert alert-info" role="alert">
+        Bienvenido <?php echo $user->getNombre(); ?>
+    </div>
+        
     </section>
     
 </body>
